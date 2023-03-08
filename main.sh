7540f63903facbd61b86a1106f376a95a4c4f534
Amir Katorza <amir.katorza@gmail.com>
2023-03-08 16:15:19 +0200

    Implemented full spectrum functions

--- main.sh
+++ main.sh
@@ -1,108 +1,126 @@
 #! /bin/bash
 
+function less_than_3() {
+    local arr=("$@")
+    local arr_len=${#arr[@]} 
+    if [[ $arr_len -lt 3 ]];
+    then    
+        echo -e "The number of arguments is not enough,\nenter at least 3 arguments"    
+        echo "0"
+    else
+        echo "1"
+    fi
+}
+
 function input_series() {
-    local var1
+    echo "Please a sequence of number seperated by spcae"
+    read sqnc
+    if [ $sqnc =~ '^[0-9 ]+$' ];
+    then
+        local input_array=(${sqnc[@]}) #($seqc)
+        echo "${input_array[@]}"
+    else
+        echo "0"
+    fi
 }
 
 function display_og_series() {
    local arr="$@"
-   for i in "${arr[@]}";
-      do
-          echo "$i"
-      done
+   echo ${arr[@]} 
 }
 
 function display_sorted_series() {
-
+    local arr="$@"
+    local sorted=$(echo "${arr[@]}" | tr ' ' '\n' | sort -n)  
+    # echo "Sorted numbers smallest to largest: "
+    echo ${sorted[@]}
 }
 
 function print_max() {
-
+    local arr="$@"
+    arr=$(display_soreted_series ${arr[@]})
+    local len=${#arr[@]} # $(series_length ${arr[@]})
+    local max_location=$(( $len-1 ))
+    local max=${arr[$max_location]}
+    echo $max
 }
 
 function print_min() {
-
+    local arr="$@"
+    arr=$(display_soreted_series ${arr[@]})
+    local min=${arr[0]}
+    echo $min
 }
 
-function print_avg() {
-
+function print_avg() { 
+    local arr="$@"
+    local sum= $(sum_series ${arr[@]})
+    local len= $(series_length ${arr[@]})
+    avg=$(echo “scale=3;$sum/$len” | bc)
+    echo "$avg" 
 }
 
 function series_length() {
-
+    local arr="$@"
+    local len=${#arr[@]}
+    echo  "$len"
 }
-
 function sum_series() {
-
+    local arr=("$@")
+    local sum=0
+    for i in ${arr[@]};
+      do
+          sum=$(( $sum + $i ))
+      done
+    echo "$sum"
 }
 
-select operation in Add Subtract Multiply Divide Power_of Modulos Exit ;
+series="$@"
+is_valid=$(less_then_3 ${series[@]})
+if [ $is_valid -eq 0 ]
+then    
+    series=$(input_series)
+    series=($series)
+fi
+
+select operation in Input_Series OG_Series Sorted_Series Series_Max Series_Min Series_AVG Series_Length Series_Sum Exit ;
 do 
     case $operation in 
-        Add ) 
-            read -p "Please enter two numbers: " num1 num2
-            chk_input=$(input_valid $num1 $num2)
-            if [ $chk_input -eq 1 ]; 
-            then
-                result=$(add $num1 $num2)
-                echo "Result: $result"
-            else
-                echo "Error: input is invalid!"
-            fi ;;
-               
-        Subtract )
-            read -p "Please enter two numbers: " num1 num2
-            chk_input=$(input_valid $num1 $num2)
-            if [ $chk_input -eq 1 ]; 
-            then
-                result=$(sub $num1 $num2)
-                echo "Result: $result"
-            else
-                echo "Error: input is invalid!"
-            fi ;;
-        Multiply ) 
-            read -p "Please enter two numbers: " num1 num2
-            chk_input=$(input_valid $num1 $num2)
-            if [ $chk_input -eq 1 ]; 
-            then
-                result=$(multiply $num1 $num2)
-                echo "Result: $result"
-            else
-                echo "Error: input is invalid!"
-            fi ;;
-        Divide ) 
-            read -p "Please enter two numbers: " num1 num2
-            chk_input=$(input_valid_div_mod $num1 $num2)
-            if [ $chk_input -eq 1 ];
-            then
-                result=$(division $num1 $num2)
-                echo "Result: $result"
-            else
-                echo "Error: input is invalid!"
-            fi ;;
-        Power_of ) 
-            read -p "Please enter two numbers: " num1 num2
-            chk_input=$(input_valid $num1 $num2)
-            if [ $chk_input -eq 1 ]; 
-            then
-                result=$(power $num1 $num2)
-                echo "Result: $result"
-            else
-                echo "Error: input is invalid!"
-            fi ;;
-        Modulos ) 
-            read -p "Please enter two numbers" num1 num2
-            chk_input=$(input_valid_div_mod $num1 $num2)
-            if [ $chk_input -eq 1 ];
-            then
-                result=$(modulos $num1 $num2)
-                echo "Result: $result"
-            else
-                echo "Error: input is invalid!"
-            fi ;;
+        Input_Series ) 
+            series=$(input_series)
+            series=($series)
+            ;;               
+        OG_Series )
+            display_og_series ${series[@]}
+            ;;
+        Sorted_Series ) 
+            display_sorted_series ${series[@]}
+            ;;
+        Series_Max ) 
+            max=$(print_max ${series[@]})
+            echo "The maximum value in the series is: $max"
+             ;;
+        Series_Min ) 
+            min=$(print_min ${series[@]})
+            echo "The maximum value in the series is: $min"
+             ;;
+         Series_AVG ) 
+            avg=$(print_avg ${series[@]})
+            echo "The average of the series is: $avg"
+             ;;
+        Series_Length )
+            len=$(print_avg ${series[@]})
+            echo "The length of the series is: $len"
+            ;;
+        Series_Sum )
+            sum=$(print_avg ${series[@]})
+            echo "The length of the series is: $sum"
+            ;;
         Exit )
-            exit ;;
+            exit 
+            ;;
         * )
-            echo "Invalid choice, try again!" ;;
+            echo "Invalid choice, try again!" 
+            ;;
     esac    
 done
